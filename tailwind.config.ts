4ff1bd75561a7f3e51c3566a79c436880a3ddbd0
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2024-07-15 23:21:10 -0400

    fixed tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,13 +18,11 @@ const config = {
       screens: {
         "2xl": "1400px",
       },
-      extend: {
-        fontFamily: {
-          sans: ["var(--font-sans)", ...fontFamily.sans],
-        },
-      },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
